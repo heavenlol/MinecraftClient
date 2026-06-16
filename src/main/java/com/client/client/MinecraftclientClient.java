621b8e54d20e761a9488b4e6186a2cee2e114ce8
heavenlol <heavenlol@users.noreply.github.com>
2026-06-16 12:02:04 -0500

    Migrate to 26.1.2 Official Mappings: GuiGraphics, Component, KeyMapping

--- src/main/java/com/client/client/MinecraftclientClient.java
+++ src/main/java/com/client/client/MinecraftclientClient.java
@@ -1,27 +1,27 @@
 package com.client.client;
 
 import com.client.client.gui.MenuScreen;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
 public class MinecraftclientClient implements ClientModInitializer {
-    private static KeyBinding guiKeyBinding;
+    private static KeyMapping guiKeyMapping;
 
     @Override
     public void onInitializeClient() {
-        guiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        guiKeyMapping = KeyBindingHelper.registerKeyBinding(new KeyMapping(
             "key.minecraftclient.open_menu",
-            InputUtil.Type.KEYSYM,
+            InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_RIGHT_SHIFT,
             "category.minecraftclient.general"
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (guiKeyBinding.wasPressed()) {
+            while (guiKeyMapping.consumeClick()) {
                 client.setScreen(new MenuScreen());
             }
         });
--- src/main/java/com/client/client/gui/MenuScreen.java
+++ src/main/java/com/client/client/gui/MenuScreen.java
@@ -1,32 +1,32 @@
 package com.client.client.gui;
 
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public class MenuScreen extends Screen {
-    private static final Identifier BACKGROUND_TEXTURE = Identifier.of("minecraftclient", "textures/gui/menu_bg.png");
+    private static final ResourceLocation BACKGROUND_TEXTURE = ResourceLocation.fromNamespaceAndPath("minecraftclient", "textures/gui/menu_bg.png");
 
     public MenuScreen() {
-        super(Text.literal("Menu"));
+        super(Component.literal("Menu"));
     }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context, mouseX, mouseY, delta);
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        super.render(graphics, mouseX, mouseY, delta);
         int x = (this.width - 200) / 2;
         int y = (this.height - 200) / 2;
-        context.drawTexture(BACKGROUND_TEXTURE, x, y, 0, 0, 200, 200, 200, 200);
+        graphics.blit(BACKGROUND_TEXTURE, x, y, 0, 0, 200, 200, 200, 200);
     }
 
     @Override
-    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderInGameBackground(context);
+    public void renderBackground(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        this.renderTransparentBackground(graphics);
     }
 
     @Override
-    public boolean shouldPause() {
+    public boolean isPauseScreen() {
         return false;
     }
 }
