f7b8375dcf99fcbbe5e4bd84cdee751aa19fd517
heavenlol <heavenlol@users.noreply.github.com>
2026-06-16 11:49:31 -0500

    Fix compilation errors: update imports and render logic

--- src/client/java/com/client/client/gui/MenuScreen.java
+++ src/client/java/com/client/client/gui/MenuScreen.java
@@ -14,10 +14,11 @@ public class MenuScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context, mouseX, mouseY, delta);
+        this.renderBackground(context, mouseX, mouseY, delta);
         int x = (this.width - 200) / 2;
         int y = (this.height - 200) / 2;
         context.drawTexture(BACKGROUND_TEXTURE, x, y, 0, 0, 200, 200, 200, 200);
+        super.render(context, mouseX, mouseY, delta);
     }
 
     @Override
